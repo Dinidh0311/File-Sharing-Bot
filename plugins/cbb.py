9defaa85a10ea9028b98595bb23412b1b882c74c
Dinidh0311 <dinidusilva500@gmail.com>
2024-05-18 17:39:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={6644409589}'>@DiniduSilva</a>\n○ Channel : @Sinhala_Cartoons_SL</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={6644409589}'>@DiniduSilva</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
